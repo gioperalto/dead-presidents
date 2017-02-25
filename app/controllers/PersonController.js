7554f9d46f08ebdfe2c470b5ef9ddd96f52c3731
gioperalto <gyoperalto@gmail.com>
2017-02-25 18:03:48 -0500

    Working on PersonController to integrate video transcription API into IBM Watson

--- app/controllers/PersonController.js
+++ app/controllers/PersonController.js
@@ -1,11 +1,12 @@
 // app/controllers/PersonController.js
 
 // Import modules
+var request = require('request');
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 var creds = require('../../config/token.js');
 var personality_insights = new PersonalityInsightsV3({
-  username: creds.username,
-  password: creds.password,
+  username: creds.ibm.username,
+  password: creds.ibm.password,
   version_date: '2016-10-20'
 });
 
@@ -13,16 +14,22 @@ var personality_insights = new PersonalityInsightsV3({
 var Person = require('../models/Person');
 
 module.exports = {
+  getSpeechTranscript: function(name, callback) {
+    request(''+name, function(error, response, body) {
+      callback(JSON.stringify(body));
+    });
+  },
 
-
-  getPersonalityInsightsFromWatson: function(callback) {
-    var params = {
-      content_items: require('./profile.json').contentItems,
-      headers: {
-        'accept-language': 'en',
-        'accept': 'application/json'
-      }
-    };
+  getPersonalityInsightsFromWatson: function(name, callback) {
+    module.exports.getSpeechTranscript(name, function(transcript) {
+      var params = {
+        text: transcript,
+        headers: {
+          'accept-language': 'en',
+          'accept': 'application/json'
+        }
+      };
+    });
 
     personality_insights.profile(params, function(error, response) {
       if (error)
