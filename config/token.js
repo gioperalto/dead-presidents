52adfcde5f7c5e70b42f48d9757574d13ead5746
gioperalto <gyoperalto@gmail.com>
2018-04-27 11:32:56 -0400

    Refactored readme, config/token.js and config/database.js

---
+++ config/token.js
@@ -0,0 +1,13 @@
+module.exports = {
+  creds: {
+    mongo: {
+      host: 'localhost',
+      port: '27017',
+      db: 'dead-presidents'
+    },
+    ibm: {
+      username: 'your_username',
+      password: 'your_password'  
+    }
+  }
+};
